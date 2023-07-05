c9c28181e86fb01c467fb50b6c851fbbb0a8f09e
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2023-07-05 15:59:53 -0700

    adds context

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Discord from 'discord.js';
+import { Client } from 'discord.js';
 import { Configuration, OpenAIApi } from 'openai';
 import minimist from 'minimist';
 
@@ -6,13 +6,13 @@ import minimist from 'minimist';
 const argv = minimist(process.argv.slice(2));
 
 // Create a new Discord client with the required intents
-const client = new Discord.Client({ intents: 32509 });
+const client = new Client();
 
 const configuration = new Configuration({
-    apiKey: argv.openaiKey,
-  });
+  apiKey: argv.openaiKey,
+});
 
-  const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 
 // Event triggered when the client is ready
 client.on('ready', () => {
@@ -21,7 +21,6 @@ client.on('ready', () => {
 
 // Event triggered when a message is received
 client.on('message', async (message) => {
-  console.log("message")
   // Ignore messages from bots
   if (message.author.bot) return;
 
@@ -33,23 +32,54 @@ client.on('message', async (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  // Handle specific commands
-  if (command === 'gpt') {
+  // Function to retrieve previous messages from a Discord channel
+  async function getPreviousMessages(channel, limit) {
+    try {
+      const messages = await channel.messages.fetch({ limit: limit });
+      return messages.map((message) => ({
+        role: 'user',
+        content: message.content
+      }));
+    } catch (error) {
+      console.error('Error retrieving previous messages:', error);
+      return [];
+    }
+  }
+
+  // Command handler
+  if (command === 'code') {
     try {
       // Join the arguments into a single input string
-      const input = args.join(' ');
+      let input = args.join(' ');
 
-      console.log(input);
+      console.log(`USER INPUT: ${input}`);
+
+      // Retrieve previous messages from the channel
+      let previousMessages = await getPreviousMessages(message.channel, 10); // Fetch the last 10 messages from the channel
+
+      // Append previous messages and user input to the messages array
+      const messages = [
+        { role: 'system', content: 'You are a helpful assistant specializing in code' },
+        ...previousMessages,
+        { role: 'user', content: input }
+      ];
 
       // Make the request to the GPT-4 model
       const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{"role": "system", "content": "You are a helpful assistant."}, {role: "user", content: input}],
+        model: 'gpt-3.5-turbo', // Replace with the desired model version
+        messages: messages,
       });
-      let response = completion.data.choices[0].message;
+      let response = completion.data.choices[0].message.content;
 
-      // Send the GPT's response back to the Discord channel
-      message.channel.send(response.choices[0].text);
+      console.log(`RESPONSE COMPLETED, LENGTH: ${response.length}`);
+
+      // Split the response into chunks
+      const chunks = response.match(/[\s\S]{1,2000}/g) || [];
+
+      // Send each chunk to the Discord channel
+      for (const chunk of chunks) {
+        message.channel.send(chunk);
+      }
     } catch (error) {
       console.error('Error:', error);
       message.channel.send('An error occurred while processing your request.');
