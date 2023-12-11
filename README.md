bcd66e4d9cf6e791e40950a1f6571c1af274156c
Tyler Bryant <tyler@nstrumenta.com>
2023-12-11 00:33:14 +0000

    adds nstrumenta ; uses env vars

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Discord Chatbot with GPT-3.5
+# Discord Chatbot with OpenAI api
 
 This is a Discord chatbot that utilizes the GPT-3.5 model from OpenAI to generate responses to messages in a Discord channel.
 
@@ -51,7 +51,7 @@ To use the chatbot, follow these steps:
 2. Start the bot:
 
    ```bash
-   node index --openaiKey=YOUR_OPENAI_API_KEY --discordToken=YOUR_DISCORD_BOT_TOKEN
+   OPENAI_API_KEY=YOUR_OPENAI_API_KEY DISCORD_TOKEN=YOUR_DISCORD_BOT_TOKEN node index
    ```
 
    Replace `YOUR_OPENAI_API_KEY` with your OpenAI API key and `YOUR_DISCORD_BOT_TOKEN` with your Discord bot token.
@@ -66,6 +66,6 @@ To use the chatbot, follow these steps:
 
    Replace `<your message>` with the text you want to send to the GPT-3.5 model.
 
-The provided code sets up a Discord chatbot that listens for messages in a channel and responds with generated text from the GPT-3.5 model. It handles the `!code` command, which triggers the bot to generate a response based on the user's input.
+The provided code sets up a Discord chatbot that listens for messages in a channel and responds with generated text from the model. It handles the `!code` command, which triggers the bot to generate a response based on the user's input.
 
-Make sure to replace the placeholders `YOUR_OPENAI_API_KEY` and `YOUR_DISCORD_BOT_TOKEN` with your actual OpenAI API key and Discord bot token, respectively.
+Make sure to set env vars or replace the placeholders `YOUR_OPENAI_API_KEY` and `YOUR_DISCORD_BOT_TOKEN` with your actual OpenAI API key and Discord bot token, respectively.
--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-import { Client } from 'discord.js';
-import { Configuration, OpenAIApi } from 'openai';
-import minimist from 'minimist';
-
-// Parse command-line arguments
-const argv = minimist(process.argv.slice(2));
+import { Client } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+import minimist from "minimist";
 
 // Create a new Discord client with the required intents
 const client = new Client();
 
 const configuration = new Configuration({
-  apiKey: argv.openaiKey,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 // Event triggered when the client is ready
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 // Event triggered when a message is received
-client.on('message', async (message) => {
+client.on("message", async (message) => {
+  console.log(message);
   // Ignore messages from bots
   if (message.author.bot) return;
 
   // Ignore messages that don't start with a specific prefix
-  const prefix = '!'; // Customize the prefix as needed
+  const prefix = "!"; // Customize the prefix as needed
   if (!message.content.startsWith(prefix)) return;
 
   // Extract the command and arguments from the message content
@@ -39,21 +37,21 @@ client.on('message', async (message) => {
       const sortedMessages = messages
         .sort((a, b) => a.createdTimestamp - b.createdTimestamp)
         .map((message) => ({
-          role: message.author.bot ? 'system' : 'user',
+          role: message.author.bot ? "system" : "user",
           content: message.content,
         }));
       return sortedMessages;
     } catch (error) {
-      console.error('Error retrieving previous messages:', error);
+      console.error("Error retrieving previous messages:", error);
       return [];
     }
   }
 
   // Command handler
-  if (command === 'code') {
+  if (command === "code") {
     try {
       // Join the arguments into a single input string
-      let input = args.join(' ');
+      let input = args.join(" ");
 
       console.log(`USER INPUT: ${input}`);
 
@@ -62,14 +60,14 @@ client.on('message', async (message) => {
 
       // Append previous messages and user input to the messages array
       const messages = [
-        { role: 'system', content: 'You are a helpful assistant named Pat' },
+        { role: "system", content: "You are a helpful assistant named chatNST. Your answers are concise and you don't add alignment boilerplate to the end of your messages" },
         ...previousMessages,
-        { role: 'user', content: input }
+        { role: "user", content: input },
       ];
 
-      // Make the request to the GPT-3.5 model
+      // Make the request to the model
       const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo', // Replace with the desired model version
+        model: "gpt-4", // Replace with the desired model version
         messages: messages,
       });
       let response = completion.data.choices[0].message.content;
@@ -84,11 +82,11 @@ client.on('message', async (message) => {
         message.channel.send(chunk);
       }
     } catch (error) {
-      console.error('Error:', error);
-      message.channel.send('An error occurred while processing your request.');
+      console.error("Error:", error);
+      message.channel.send("An error occurred while processing your request.");
     }
   }
 });
 
 // Log in to Discord using your bot token
-client.login(argv.discordToken);
+client.login(process.env.DISCORD_TOKEN);
